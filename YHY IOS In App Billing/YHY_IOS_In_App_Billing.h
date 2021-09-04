36ad4542555a6c30f3dae8488a419a95d574a8a0
Ucdemir <dortsekiz48@gmail.com>
2021-09-04 06:05:02 -0700

    Initial Commit

---
+++ YHY IOS In App Billing/YHY_IOS_In_App_Billing.h
@@ -0,0 +1,18 @@
+//
+//  YHY_IOS_In_App_Billing.h
+//  YHY IOS In App Billing
+//
+//  Created by Ucdemir on 4.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YHY_IOS_In_App_Billing.
+FOUNDATION_EXPORT double YHY_IOS_In_App_BillingVersionNumber;
+
+//! Project version string for YHY_IOS_In_App_Billing.
+FOUNDATION_EXPORT const unsigned char YHY_IOS_In_App_BillingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YHY_IOS_In_App_Billing/PublicHeader.h>
+
+
---
+++ YHY IOS In App BillingTests/YHY_IOS_In_App_BillingTests.swift
@@ -0,0 +1,33 @@
+//
+//  YHY_IOS_In_App_BillingTests.swift
+//  YHY IOS In App BillingTests
+//
+//  Created by Ucdemir on 4.09.2021.
+//
+
+import XCTest
+@testable import YHY_IOS_In_App_Billing
+
+class YHY_IOS_In_App_BillingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
