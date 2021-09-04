2e36aeab3db77eea502a714c737fed41dfeb8f39
Ucdemir <dortsekiz48@gmail.com>
2021-09-04 07:24:20 -0700

    initial

---
+++ YHY IOS In App Billing/test.swift
@@ -0,0 +1,8 @@
+//
+//  test.swift
+//  YHY IOS In App Billing
+//
+//  Created by Ucdemir on 4.09.2021.
+//
+
+import Foundation
---
+++ test.swift
@@ -0,0 +1,29 @@
+//
+//  test.swift
+//  YHY IOS In App Billing
+//
+//  Created by Ucdemir on 4.09.2021.
+//
+
+import UIKit
+
+class test: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
