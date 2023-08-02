9cfbbb7353501798775b0b11bdd9b8dfc2b68812
Ucdemir <dortsekiz48@gmail.com>
2023-08-02 18:35:39 +0300

    test

---
+++ Tests/YHY IOS In App BillingTests/YHY_IOS_In_App_BillingTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import YHY_IOS_In_App_Billing
+
+final class YHY_IOS_In_App_BillingTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
